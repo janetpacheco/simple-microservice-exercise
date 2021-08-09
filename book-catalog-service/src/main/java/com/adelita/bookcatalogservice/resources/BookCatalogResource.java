ffd89ea208655ebeec26f0f9cd9da729ba7bc115
janetpacheco <anne.janeta@gmail.com>
2021-08-08 23:42:45 -0500

    03.  Added Book Info API

--- book-catalog-service/src/main/java/com/adelita/bookcatalogservice/resources/BookCatalogResource.java
+++ book-catalog-service/src/main/java/com/adelita/bookcatalogservice/resources/BookCatalogResource.java
@@ -3,7 +3,6 @@ package com.adelita.bookcatalogservice.resources;
 import java.util.Collections;
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +13,7 @@ import com.adelita.bookcatalogservice.models.CatalogItem;
 @RequestMapping("/catalog")
 public class BookCatalogResource {
 	
-	@GetMapping("/{userId}")
+	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 		return Collections.singletonList(new CatalogItem("The shadow of the wind", "Test", 5));
 	}
---
+++ book-info-service/src/main/java/com/adelita/bookinfoservice/resources/BookResource.java
@@ -0,0 +1,18 @@
+package com.adelita.bookinfoservice.resources;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.adelita.bookinfoservice.models.Book;
+
+@RestController
+@RequestMapping("/books")
+
+public class BookResource {
+	@RequestMapping("/{bookId}")
+	public	Book getBookInfo(@PathVariable("bookId") String bookId) {		
+		return new Book(bookId,"Test name");
+	 }
+
+}
