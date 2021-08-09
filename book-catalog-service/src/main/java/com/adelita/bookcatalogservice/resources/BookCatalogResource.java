69d840c5caa61c7d9d70507efeb299630b8c5b7d
janetpacheco <anne.janeta@gmail.com>
2021-08-08 22:28:54 -0500

    02. Added Book Catalog Service API

---
+++ book-catalog-service/src/main/java/com/adelita/bookcatalogservice/resources/BookCatalogResource.java
@@ -0,0 +1,23 @@
+package com.adelita.bookcatalogservice.resources;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.adelita.bookcatalogservice.models.CatalogItem;
+
+@RestController
+@RequestMapping("/catalog")
+public class BookCatalogResource {
+	
+	@GetMapping("/{userId}")
+	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+		return Collections.singletonList(new CatalogItem("The shadow of the wind", "Test", 5));
+	}
+	
+
+}
